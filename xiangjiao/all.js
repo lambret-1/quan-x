6c0a325b474223bb5c0e76317d2d2f4dad1b74c5
Lambret <lambret2025@163.com>
2026-09-21 23:16:47 +0800

    Update all.js

--- xiangjiao/all.js
+++ xiangjiao/all.js
@@ -1,4 +1,9 @@
 [rewrite_local]
-^https?:\/\/wx\.10086\.cn\/qwhdhub\/ url script-request-header https://raw.githubusercontent.com/lambret-1/quan-x/main/xiangjiao/ad.js
+
+^https?://h5\.xxoox20\.org/api/init url script-response-body https://raw.githubusercontent.com/lambret-1/quan-x/main/xiangjiao/ad.js
+
+^https?://h5\.xxoox20\.org/api/index url script-response-body https://raw.githubusercontent.com/lambret-1/quan-x/main/xiangjiao/ad1.js
+
+
 [MITM]
-hostname = %APPEND% wx.10086.cn
+hostname = %APPEND% h5.xxoox20.org
